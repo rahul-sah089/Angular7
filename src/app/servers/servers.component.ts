2c6459a4664e2305f77ef109d33b27b79fe66f84
rahul.sah089 <rahul.sah089@gmail.com>
2018-12-30 00:30:14 +0530

    simple login page,validation using data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,26 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer:boolean = false;
-  serverCreationStatus : String  = "No server was createds";
+  allowNewServer: boolean = false;
+  serverCreationStatus: String = "No server was createds";
   serverName1 = "";
   serverName2 = "Test Server";
+  userName = "";
+  password = "";
+  showUserName = false;
+  showPassword = false;
   constructor() {
-    setTimeout(() => { 
+    setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
   ngOnInit() {
   }
 
-  onSeverCreation(){
-    this.serverCreationStatus = "Server Created and the server name is "+this.serverName2;
+  onSeverCreation() {
+    this.serverCreationStatus = "Server Created and the server name is " + this.serverName2;
   }
 
-  onUpdateServerName(values:Event){
+  login() {
+    //simple validation for username. Avoid empty value
+    if (this.userName.length < 2) {
+      this.showUserName = true;
+      return;
+    }
+    ///simple validation for password. Avoid empty value
+    if (this.password.length < 3) {
+      this.showPassword = true;
+      return;
+    }
+    alert("login successful");
+    console.log("Username " + this.userName + " and password " + this.password);
+    //reset the input fields
+    this.userName = "";
+    this.password = "";
+
+  }
+
+  onUpdateServerName(values: Event) {
     //alert("update server called");
     console.log(values);
-    this.serverName1 = (<HTMLInputElement>values.target).value; 
+    this.serverName1 = (<HTMLInputElement>values.target).value;
   }
 
 }
