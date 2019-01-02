b78a35e2425fe3c69d01b852b64a637c5c7a9951
rahul.sah089 <rahul.sah089@gmail.com>
2019-01-02 09:03:03 +0530

    binding to coustom event

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./databinding.component.css']
 })
 export class DataBinding {
-  serverElements = [{type:'server',name:'TestServer',content:'Jest a test!!!'}];
+  serverElements = [{ type: 'server', name: 'TestServer', content: 'Jest a test!!!' }];
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push(
+      {
+        type: "server",
+        name: serverData.serverName,
+        content: serverData.serverContent
+      });
+  }
+
+  onBlueprintAdded(bluePrintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
+
+
 }
--- src/app/serverelement/serverelement.component.ts
+++ src/app/serverelement/serverelement.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./serverelement.component.css']
 })
 export class ServerelementComponent implements OnInit {
-  @Input() element: { type: string, name: string, content: string };
+  @Input("srvElement") element: { type: string, name: string, content: string };
   constructor() { }
 
   ngOnInit() {
